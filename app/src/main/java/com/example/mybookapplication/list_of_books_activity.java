150930500c40a5f97f93d61168a10778e916cbab
Kaneki_Ken 05 <ccartijano@gmail.com>
2023-11-26 19:30:27 +0800

    Initial commit

---
+++ app/src/main/java/com/example/mybookapplication/list_of_books_activity.java
@@ -0,0 +1,151 @@
+
+	 
+	/*
+	 *	This content is generated from the API File Info.
+	 *	(Alt+Shift+Ctrl+I).
+	 *
+	 *	@desc 		
+	 *	@file 		list_of_books
+	 *	@date 		Saturday 25th of November 2023 11:08:28 AM
+	 *	@title 		Page 1
+	 *	@author 	
+	 *	@keywords 	
+	 *	@generator 	Export Kit v1.3.figma
+	 *
+	 */
+	
+
+package com.example.mybookapplication;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+import android.view.View;
+import android.widget.TextView;
+import android.widget.ImageView;
+
+public class list_of_books_activity extends Activity {
+
+	
+	private View _bg__list_of_books_ek2;
+	private View _bg__button_ek1;
+	private TextView one_piece;
+	private View _bg__icon_ek1;
+	private ImageView vector_88;
+	private View _bg__heart_ek1;
+	private View rectangle_1;
+	private View rectangle_2;
+	private View _bg__button_ek3;
+	private TextView bleach;
+	private View _bg__icon_ek3;
+	private ImageView vector_88_ek1;
+	private final View _bg__heart_ek3 = findViewById(R.id._bg__heart_ek3);
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.list_of_books);
+
+		
+		_bg__list_of_books_ek2 = findViewById(R.id._bg__list_of_books_ek2);
+		_bg__button_ek1 = findViewById(R.id._bg__button_ek1);
+		one_piece = findViewById(R.id.one_piece);
+		_bg__icon_ek1 = findViewById(R.id._bg__icon_ek1);
+		vector_88 = findViewById(R.id.vector_88);
+		_bg__heart_ek1 = findViewById(R.id._bg__heart_ek1);
+		rectangle_1 = findViewById(R.id.rectangle_1);
+		rectangle_2 = findViewById(R.id.rectangle_2);
+		_bg__button_ek3 = findViewById(R.id._bg__button_ek3);
+		bleach = findViewById(R.id.bleach);
+		_bg__icon_ek3 = findViewById(R.id._bg__icon_ek3);
+		vector_88_ek1 = findViewById(R.id.vector_88_ek1);
+		findViewById(R.id.rectangle_1_ek1);
+		findViewById(R.id.rectangle_2_ek1);
+		findViewById(R.id._bg__button_ek5);
+		findViewById(R.id.my_hero_academia);
+		findViewById(R.id._bg__icon_ek5);
+		findViewById(R.id.vector_88_ek2);
+		findViewById(R.id._bg__heart_ek5);
+		findViewById(R.id.rectangle_1_ek2);
+		findViewById(R.id.rectangle_2_ek2);
+		findViewById(R.id._bg__button_ek7);
+		 findViewById(R.id.black_clover);
+		findViewById(R.id._bg__icon_ek7);
+		findViewById(R.id.vector_88_ek3);
+		findViewById(R.id._bg__heart_ek7);
+		findViewById(R.id.rectangle_1_ek3);
+		findViewById(R.id.rectangle_2_ek3);
+		findViewById(R.id._bg__button_ek9);
+		findViewById(R.id.solo_leveling);
+		findViewById(R.id._bg__icon_ek9);
+		findViewById(R.id.vector_88_ek4);
+		findViewById(R.id._bg__heart_ek9);
+		findViewById(R.id.rectangle_1_ek4);
+		findViewById(R.id.rectangle_2_ek4);
+		findViewById(R.id.list_of_anime_books__manga_);
+	
+		
+		//custom code goes here
+	
+	}
+
+	public View get_bg__heart_ek3() {
+		return _bg__heart_ek3;
+	}
+
+	public ImageView getVector_88_ek1() {
+		return vector_88_ek1;
+	}
+
+	public void setVector_88_ek1(ImageView vector_88_ek1) {
+		this.vector_88_ek1 = vector_88_ek1;
+	}
+
+	public View get_bg__icon_ek3() {
+		return _bg__icon_ek3;
+	}
+
+	public TextView getBleach() {
+		return bleach;
+	}
+
+	public View get_bg__button_ek3() {
+		return _bg__button_ek3;
+	}
+
+	public View getRectangle_2() {
+		return rectangle_2;
+	}
+
+	public View getRectangle_1() {
+		return rectangle_1;
+	}
+
+	public View get_bg__heart_ek1() {
+		return _bg__heart_ek1;
+	}
+
+	public ImageView getVector_88() {
+		return vector_88;
+	}
+
+	public View get_bg__icon_ek1() {
+		return _bg__icon_ek1;
+	}
+
+	public TextView getOne_piece() {
+		return one_piece;
+	}
+
+	public View get_bg__button_ek1() {
+		return _bg__button_ek1;
+	}
+
+	public View get_bg__list_of_books_ek2() {
+		return _bg__list_of_books_ek2;
+	}
+}
+	
+	
